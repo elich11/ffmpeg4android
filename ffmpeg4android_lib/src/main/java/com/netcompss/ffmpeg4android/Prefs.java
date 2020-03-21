799d46d971a142a103fc7fdf2b83d95066ea5f40
eli_h <eli.hasson@netcompss.com>
2020-03-21 11:57:32 +0200

    Added android:extractNativeLibs=“true” to the demo app manifest.

--- ffmpeg4android_lib/src/main/java/com/netcompss/ffmpeg4android/Prefs.java
+++ ffmpeg4android_lib/src/main/java/com/netcompss/ffmpeg4android/Prefs.java
@@ -2,5 +2,5 @@ package com.netcompss.ffmpeg4android;
 
 public class Prefs {
 	public static final String TAG = "ffmpeg4android";
-	public static final String version = "41.06.00_LM41_00";
+	public static final String version = "41.08.00_LM41_00";
 }
